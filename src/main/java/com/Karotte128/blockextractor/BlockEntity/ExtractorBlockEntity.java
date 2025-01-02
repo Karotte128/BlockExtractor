883ebe602f24fa07bb8189d44af2bca408f9b526
Karotte128 <karotte128@outlook.de>
2025-01-02 20:30:25 +0100

    first extractor experiment

--- src/main/java/com/Karotte128/blockextractor/BlockEntity/ExtractorBlockEntity.java
+++ src/main/java/com/Karotte128/blockextractor/BlockEntity/ExtractorBlockEntity.java
@@ -1,11 +1,29 @@
 package com.Karotte128.blockextractor.BlockEntity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class ExtractorBlockEntity extends BlockEntity {
     public ExtractorBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.EXTRACTOR_BLOCK_ENTITY.get(), pos, state);
+    }
+
+    private static int tickCounter = 0;
+
+    public static <T extends BlockEntity> void tick(Level level, BlockPos blockPos, BlockState blockState, T t) {
+
+        ExtractorBlockEntity.tickCounter++;
+
+        if (ExtractorBlockEntity.tickCounter == 40) {
+            BlockPos belowPos = blockPos.below();
+            BlockState belowBlockState = level.getBlockState(belowPos);
+            ItemStack itemStack = new ItemStack(belowBlockState.getBlock().asItem());
+            level.addFreshEntity(new ItemEntity(level, blockPos.getX() + 0.5, blockPos.getY() + 1, blockPos.getZ() + 0.5, itemStack));
+            ExtractorBlockEntity.tickCounter = 0;
+        }
     }
 }
--- src/main/java/com/Karotte128/blockextractor/block/ExtractorBlock.java
+++ src/main/java/com/Karotte128/blockextractor/block/ExtractorBlock.java
@@ -1,10 +1,14 @@
 package com.Karotte128.blockextractor.block;
 
 import com.Karotte128.blockextractor.BlockEntity.ExtractorBlockEntity;
+import com.Karotte128.blockextractor.BlockEntity.ModBlockEntities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,5 +21,11 @@ public class ExtractorBlock extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new ExtractorBlockEntity(blockPos, blockState);
+    }
+
+    @SuppressWarnings("unchecked") // Due to generics, an unchecked cast is necessary here.
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return type == ModBlockEntities.EXTRACTOR_BLOCK_ENTITY.get() ? (BlockEntityTicker<T>) ExtractorBlockEntity::tick : null;
     }
 }
